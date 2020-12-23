f077c2708afe6c013305f17be050661e12e9d779
Razvan <simtionrazvan7@gmail.com>
2020-12-23 23:35:43 +0200

    repositories

---
+++ repositories/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const UsersRepository = require('./users.repository')
+const ProjectsRepository = require('./projects.repository')
+
+module.exports = {
+  UsersRepository,
+  ProjectsRepository,
+}

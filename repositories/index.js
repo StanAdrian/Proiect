d26c2df36c919505ece435e1914d9ecee95a6899
Adrian Stan <61725053+StanAdrian@users.noreply.github.com>
2021-01-18 21:44:29 +0200

    Delete repositories directory

--- repositories/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const UsersRepository = require('./users.repository')
-const ProjectsRepository = require('./projects.repository')
-
-module.exports = {
-  UsersRepository,
-  ProjectsRepository,
-}
